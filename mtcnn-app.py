f1396748c689ae80b72f5588d8f60bb6f1c6500f
Pavan Kunchala <pavankunchalapk@gmail.com>
2021-05-10 14:07:00 +0530

    Add files via upload

--- mtcnn-app.py
+++ mtcnn-app.py
@@ -38,8 +38,8 @@ def blurFace(image,detector,confidence):
             x, y, width, height = det['box']
             keypoints = det['keypoints']
             cv2.rectangle(image, (x,y), (x+width,y+height), (0,0,255), 3)
-            cv2.putText(image,"{},{},{}".format(det['box'][2],det['box'][3],det['confidence']),(x,y),cv2.FONT_HERSHEY_COMPLEX,0.6,(0,255,255),2)
-            roi = image[y:y+height,x:x+width]
+            cv2.putText(image,"{},{},{:.2f}".format(det['box'][2],det['box'][3],det['confidence']),(x,y),cv2.FONT_HERSHEY_COMPLEX,0.6,(0,255,255),2)
+            roi = image[y:y+height - height/2,x:x+width]
             
             roi = cv2.GaussianBlur(roi,(23,23),20)
             
